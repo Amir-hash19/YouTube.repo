b232bd1747caf872b342dad7321e3f8fc7a29519
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-07-14 00:49:06 +0330

    add models for user_managment app

--- YouTube/YouTube/settings.py
+++ YouTube/YouTube/settings.py
@@ -146,3 +146,8 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
     "AUTH_HEADER_TYPES": ("Bearer", ),
 }
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- YouTube/user_managment/models.py
+++ YouTube/user_managment/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+import re
 
-# Create your models here.
+
+
+
+def validate_username_with_special_characters(value):
+    if re.match(r'^[a-zA-Z0-9]*$', value):
+        raise ValidationError("Username must contain at least one special charactes")
+
+
+def password_validator(value):
+    if len(value) != 8 and not re.match(r'^[a-zA-Z0-9]*$', value):
+        raise ValidationError("National ID must be exactly 10 digits.")
+
+
+
+
+class UserAccount(models.Model):
+    username = models.CharField(unique=True, max_length=100, validators=[validate_username_with_special_characters])
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(validators=[password_validator], max_length=8)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+    birthday = models.DateField()
+
+    GENDER_TYPE = (
+        ("female", "FEMALE"),
+        ("male", "MALE")
+    )
+
+    gender = models.CharField(max_length=6, choices=GENDER_TYPE, null=True, blank=True)
+    slug = models.SlugField(unique=True)
+
+
+
+
+
+
+class SocialLink(models.Model):
+    PLATFORM_CHOICES = [
+        ('twitter', 'Twitter'),
+        ('instagram', 'Instagram'),
+        ('youtube', 'YouTube'),
+        ('linkedin', 'LinkedIn'),
+        ('website', 'Website'),
+        
+    ]
+
+    user = models.ForeignKey(to=UserAccount, on_delete=models.CASCADE)
+    platform = models.CharField(max_length=50,  choices=PLATFORM_CHOICES)
+    url = models.URLFIELD()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.platform}"
+
+
+
+
+
+
+class UserAvatar(models.Model):
+    user = models.ForeginKey(to=UserAccount,  on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='avatars/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.slug}"
