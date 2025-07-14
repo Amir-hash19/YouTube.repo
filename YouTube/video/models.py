d171475c2cdcd37f4c3f8272dee6432ff83bad44
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-07-14 17:33:41 +0330

    add models for video and channel apps

--- YouTube/video/models.py
+++ YouTube/video/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from channle.models import Channel
 
-# Create your models here.
+
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    caption = models.TextField(blank=True)
+    channel = models.ForeignKey(to=Channel, on_delete=models.CASCADE, related_name="videos")
+    video = models.FileField(upload_to="content/videos/")
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    #comments
+    #likes/dislikes
+    slug = models.SlugField(unique=True)
+
+
+    def __str__(self):
+        return f"{self.title}"
+
+
