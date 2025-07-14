1e54e5310dcf324b7bc544277f279f1e01f24374
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2025-07-14 23:36:20 +0330

    add models fields for comments_like, video, channel

--- YouTube/video/models.py
+++ YouTube/video/models.py
@@ -4,16 +4,57 @@ from channle.models import Channel
 
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
 class Video(models.Model):
     title = models.CharField(max_length=255)
     caption = models.TextField(blank=True)
     channel = models.ForeignKey(to=Channel, on_delete=models.CASCADE, related_name="videos")
     video = models.FileField(upload_to="content/videos/")
     date_uploaded = models.DateTimeField(auto_now_add=True)
+    duration = models.DurationField(max_length=100)
+
+    VIDEO_STATUS = (
+        ("draft", "DRAFT"),
+        ("published","PUBLISHED")
+    )
+    video_status = models.CharField(max_length=20, choices=VIDEO_STATUS, default="draft")
+
     #comments
     #likes/dislikes
-    slug = models.SlugField(unique=True)
+    tags = models.ManyToManyField(to=Tag, blank=True,related_name="videos_tag")
+    slug = models.SlugField(unique=True, max_length=255)
 
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+
+
+
+
+
+class PlayList(models.Model):
+    channel = models.ForeignKey(to=Channel, on_delete=models.CASCADE)
+    title = models.CharField(max_length=225)
+    video = models.ManyToManyField(to=Video, related_name="videos_Playlist")
+    description = models.TextField(null=True, blank=True)
+
+    PLAYLIST_STATUS = (
+        ("draft", "DRAFT"),
+        ("published","PUBLISHED")
+    )
+
+    playlist_status = models.CharField(max_length=20, choices=PLAYLIST_STATUS)
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return f"{self.title}"
