d992798dc1293044190ca238b407a15f612d0aec
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2025-07-14 16:41:32 +0330

    add models for user_managment done

--- YouTube/user_managment/models.py
+++ YouTube/user_managment/models.py
@@ -1,5 +1,56 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils.text import slugify
 import re
+from .exceptions import MissingEmailError, EmailAlreadyExistsError, InvalidGenderError, MissingUsernameError, UsernameAlreadyExistsError, WeakPasswordError
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, username=None, birthday=None, gender=None):
+        if not email:
+            raise MissingEmailError()
+
+        if self.model.objects.filter(email=email).exists():
+            raise EmailAlreadyExistsError()
+        
+        email = self.normalize_email(email)
+
+        if len(password) != 8:
+            raise WeakPasswordError()
+        
+        allowed_genders = ["male", "female"]
+        if gender and gender not in allowed_genders:
+            raise InvalidGenderError()
+        
+
+        user = self.model(
+            email=email,
+            username=username,
+            birthday=birthday,
+            gender=gender,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email,password=None, **extra_fields):
+        if not username:
+            raise MissingUsernameError()
+        
+        if self.model.objects.filter(username=username).exists():
+            raise UsernameAlreadyExistsError()
+        
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        
+        user = self.model(username=username, email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+        
 
 
 
@@ -9,22 +60,19 @@ def validate_username_with_special_characters(value):
         raise ValidationError("Username must contain at least one special charactes")
 
 
-def password_validator(value):
-    if len(value) != 8 and not re.match(r'^[a-zA-Z0-9]*$', value):
-        raise ValidationError("National ID must be exactly 10 digits.")
 
 
 
-
-class UserAccount(models.Model):
+class UserAccount(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(unique=True, max_length=100, validators=[validate_username_with_special_characters])
-    full_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    password = models.CharField(validators=[password_validator], max_length=8)
+    password = models.CharField(max_length=225)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_added = models.DateTimeField(auto_now_add=True)
-    birthday = models.DateField()
+    is_advertiser = models.BooleanField(default=False)
+    is_channel_admin = models.BooleanField(default=False)
+    birthday = models.DateField(null=True, blank=True)
 
     GENDER_TYPE = (
         ("female", "FEMALE"),
@@ -35,37 +83,31 @@ class UserAccount(models.Model):
     slug = models.SlugField(unique=True)
 
 
+    objects = CustomUserManager()
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email", "password"]
 
-
-
-
-class SocialLink(models.Model):
-    PLATFORM_CHOICES = [
-        ('twitter', 'Twitter'),
-        ('instagram', 'Instagram'),
-        ('youtube', 'YouTube'),
-        ('linkedin', 'LinkedIn'),
-        ('website', 'Website'),
-        
-    ]
-
-    user = models.ForeignKey(to=UserAccount, on_delete=models.CASCADE)
-    platform = models.CharField(max_length=50,  choices=PLATFORM_CHOICES)
-    url = models.URLFIELD()
+    def get_username(self):
+        return f"The Username is  {self.username}"
 
     def __str__(self):
-        return f"{self.user.username} - {self.platform}"
-
+        return f"{self.username}"
 
 
 
 
 
 class UserAvatar(models.Model):
-    user = models.ForeginKey(to=UserAccount,  on_delete=models.CASCADE)
+    user = models.ForeignKey(to=UserAccount,  on_delete=models.CASCADE)
     image = models.ImageField(upload_to='avatars/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(unique=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.slug}"
+
+
+
+
+
+
